5b8ec826c181cfcb95cd69b66456bce313992ae4
spooketti <jonliu168@gmail.com>
2024-03-20 14:24:10 -0700

    statitsics

--- src/post.js
+++ src/post.js
@@ -1,6 +1,6 @@
 let fileUpload = document.getElementById("faceUpload")
 let photoPreview = document.getElementById("uploadPreview")
-let endpoint = "http://192.168.1.106:6221/"
+let endpoint = "http://10.96.0.119:6221/getPrediction/"
 fileUpload.addEventListener('change', function(event) {
     pfpChanged = true
     const file = event.target.files[0]; // Get the selected file
@@ -38,12 +38,25 @@ function sendFace()
         }).then(response =>{
             if(response.ok)
             {
-                chatbox.value = null;
+                //chatbox.value = null;
                 return response.json()
             }
             throw new Error("Network response failed")
         }).then(data => {
-            console.log("Response:", data);
+            //console.log("Response:", data);
+            let img = new Image()
+            img.src = data["image"]
+            img.onload = function()
+            {
+                ctx.drawImage(img,0,0,canvas.width,canvas.height)
+            }
+            for(let i=0;i<8;i++)
+            {
+                let doc = document.getElementById(`s${i}`)
+                doc.innerText = data[i.toString()]
+            }
+            document.getElementById("gconf").innerText = data["gconf"]
+            
           })
           .catch(error => {
             console.error("There was a problem with the fetch", error);
